896cc485a8fca96bb061c57ac8173ed0cd0ccb74
NarasetS <80676034+NarasetS@users.noreply.github.com>
2023-11-08 14:55:57 +0700

    ""

---
+++ Run.py
@@ -0,0 +1,20 @@
+import PSDCODAPIFUNCS as psd
+import json
+import pandas as pd
+from pandas import json_normalize
+
+### Usernamepassword input format as "username:password" ##
+usernamepassword = open('usernamepassword.txt','r')
+usernamepassword = usernamepassword.read()
+usernamepasswordbase64 = psd.converttobase64(usernamepassword)
+token = psd.gettoken(usernamepasswordbase64)
+
+### Date input format as "Date/Month/Year"
+startdate = '01/01/2023'
+enddate = '31/01/2023'
+
+dt_dailyfueldata = psd.importdailyfuel(startdate,enddate,token)
+dt_dailyfueldata.to_csv('DailyFuelData_'+startdate.replace('/','-')+'_to_'+enddate.replace('/','-')+'.csv',encoding="utf-8-sig",index=False)
+
+dt_dailyenergycompare = psd.importdailyenergy(startdate,enddate,token)
+dt_dailyenergycompare.to_csv('DailyEnergyData_'+startdate.replace('/','-')+'_to_'+enddate.replace('/','-')+'.csv',encoding="utf-8-sig",index=False)
